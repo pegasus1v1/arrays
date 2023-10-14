37e485f64ba1a9360f788d69843cfe5ea0e487fe
Joseph <113266325+pegasus1v1@users.noreply.github.com>
2023-10-14 12:29:06 -0500

    Add files via upload

---
+++ videos arra/1.js
@@ -0,0 +1,6 @@
+const listaAnime = [
+    {
+        nombre: "Los Simpsin 1x1",
+        url: "https://www.youtube.com/embed/TtKA6OzATbY?si=3qPT6-VpupVGVbOy",
+    }
+]
---
+++ videos arra/2.js
@@ -0,0 +1,6 @@
+const listaAnime = [
+    {
+        nombre: "Los Simpsin 1x1",
+        url: "https://www.blogger.com/video.g?token=AD6v5dy8LMVJy-IEFOm1WRQ6tpH8Gp0isBdZpi_OytoLz3LkImgVQ81jSP7hbHWUzh4OsPsc-NquU_sHrI_4yxDCzInG0t6Uc90jsyPQUfcQBD3MgEccMeT9I-G0YYjE5eGbtuhZ0INy",
+    }
+]
