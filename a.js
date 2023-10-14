b57baec524bcc5be844c8b38f1f45637f8093e7b
Joseph <113266325+pegasus1v1@users.noreply.github.com>
2023-10-14 12:30:55 -0500

    Add files via upload

---
+++ a.js
@@ -0,0 +1,50 @@
+const listaAnime = [
+    {
+        nombre: "Los Simpsin 1x1",
+        url: "https://www.youtube.com/embed/TtKA6OzATbY?si=3qPT6-VpupVGVbOy",
+    },
+    {
+        nombre: "Los Simpsin 1x2",
+        url: "https://www.blogger.com/video.g?token=AD6v5dwfgZpz_jqUhUKiu3QRnCC-HHqFh9LfA5ULsY9A3YnxkL_21IY7se_11w3xS4yqic0dFPu0NsvOj9xkYgfxxN3n1l0rsBVLVzTGaIpAPFS-oin6WaCxbbHCaY8buX6ub_XPE74W",
+    },
+    {
+        nombre: "Los Simpsin 1x2",
+        url: "https://www.blogger.com/video.g?token=AD6v5dwfgZpz_jqUhUKiu3QRnCC-HHqFh9LfA5ULsY9A3YnxkL_21IY7se_11w3xS4yqic0dFPu0NsvOj9xkYgfxxN3n1l0rsBVLVzTGaIpAPFS-oin6WaCxbbHCaY8buX6ub_XPE74W",
+    },
+    {
+        nombre: "Los Simpsin 1x2",
+        url: "https://www.blogger.com/video.g?token=AD6v5dwfgZpz_jqUhUKiu3QRnCC-HHqFh9LfA5ULsY9A3YnxkL_21IY7se_11w3xS4yqic0dFPu0NsvOj9xkYgfxxN3n1l0rsBVLVzTGaIpAPFS-oin6WaCxbbHCaY8buX6ub_XPE74W",
+    },
+    {
+        nombre: "Los Simpsin 1x2",
+        url: "https://www.blogger.com/video.g?token=AD6v5dwfgZpz_jqUhUKiu3QRnCC-HHqFh9LfA5ULsY9A3YnxkL_21IY7se_11w3xS4yqic0dFPu0NsvOj9xkYgfxxN3n1l0rsBVLVzTGaIpAPFS-oin6WaCxbbHCaY8buX6ub_XPE74W",
+    },
+    {
+        nombre: "Los Simpsin 1x2",
+        url: "https://www.blogger.com/video.g?token=AD6v5dwfgZpz_jqUhUKiu3QRnCC-HHqFh9LfA5ULsY9A3YnxkL_21IY7se_11w3xS4yqic0dFPu0NsvOj9xkYgfxxN3n1l0rsBVLVzTGaIpAPFS-oin6WaCxbbHCaY8buX6ub_XPE74W",
+    },
+    {
+        nombre: "Los Simpsin 1x2",
+        url: "https://www.blogger.com/video.g?token=AD6v5dwfgZpz_jqUhUKiu3QRnCC-HHqFh9LfA5ULsY9A3YnxkL_21IY7se_11w3xS4yqic0dFPu0NsvOj9xkYgfxxN3n1l0rsBVLVzTGaIpAPFS-oin6WaCxbbHCaY8buX6ub_XPE74W",
+    },
+    {
+        nombre: "Los Simpsin 1x2",
+        url: "https://www.blogger.com/video.g?token=AD6v5dwfgZpz_jqUhUKiu3QRnCC-HHqFh9LfA5ULsY9A3YnxkL_21IY7se_11w3xS4yqic0dFPu0NsvOj9xkYgfxxN3n1l0rsBVLVzTGaIpAPFS-oin6WaCxbbHCaY8buX6ub_XPE74W",
+    },
+    {
+        nombre: "Los Simpsin 1x2",
+        url: "https://www.blogger.com/video.g?token=AD6v5dwfgZpz_jqUhUKiu3QRnCC-HHqFh9LfA5ULsY9A3YnxkL_21IY7se_11w3xS4yqic0dFPu0NsvOj9xkYgfxxN3n1l0rsBVLVzTGaIpAPFS-oin6WaCxbbHCaY8buX6ub_XPE74W",
+    },
+    {
+        nombre: "Los Simpsin 1x2",
+        url: "https://www.blogger.com/video.g?token=AD6v5dwfgZpz_jqUhUKiu3QRnCC-HHqFh9LfA5ULsY9A3YnxkL_21IY7se_11w3xS4yqic0dFPu0NsvOj9xkYgfxxN3n1l0rsBVLVzTGaIpAPFS-oin6WaCxbbHCaY8buX6ub_XPE74W",
+    },
+    {
+        nombre: "Los Simpsin 1x2",
+        url: "https://www.blogger.com/video.g?token=AD6v5dwfgZpz_jqUhUKiu3QRnCC-HHqFh9LfA5ULsY9A3YnxkL_21IY7se_11w3xS4yqic0dFPu0NsvOj9xkYgfxxN3n1l0rsBVLVzTGaIpAPFS-oin6WaCxbbHCaY8buX6ub_XPE74W",
+    },
+    {
+        nombre: "Los Simpsin 1x2",
+        url: "https://www.blogger.com/video.g?token=AD6v5dwfgZpz_jqUhUKiu3QRnCC-HHqFh9LfA5ULsY9A3YnxkL_21IY7se_11w3xS4yqic0dFPu0NsvOj9xkYgfxxN3n1l0rsBVLVzTGaIpAPFS-oin6WaCxbbHCaY8buX6ub_XPE74W",
+    }
+]
